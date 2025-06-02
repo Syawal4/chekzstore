55cde3c9f3a14d42a6b03e2243a9b4c5bc902250
Syawal4 <syawaal44@gmail.com>
2025-06-02 21:44:08 +0800

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-// Toggle class active
 const navbarMenu = document.querySelector(".navbar-menu");
 const shoppingCart = document.querySelector(".shopping-cart");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-// Ketika menu diklik
 document.querySelector("#menu-button").onclick = (e) => {
   navbarMenu.classList.toggle("active");
   e.preventDefault();
@@ -12,13 +12,24 @@ document.querySelector("#shopping-cart-button").onclick = (e) => {
   e.preventDefault();
 };
 
-// klik di luar elemen untuk menutup
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus;
+  e.preventDefault();
+};
+
 const menu = document.querySelector("#menu-button");
 const sc = document.querySelector("#shopping-cart-button");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarMenu.contains(e.target)) {
     navbarMenu.classList.remove("active");
+  }
+});
+document.addEventListener("click", function (e) {
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
 document.addEventListener("click", function (e) {
